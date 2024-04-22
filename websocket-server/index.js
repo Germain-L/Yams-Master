a7094c9174840dbc9e58ed7fcc27ae64e22ddd05
Germain-L <germain.leignel@gmail.com>
2024-04-22 15:21:17 +0200

    server

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require("socket.io")(server, {
+const io = require("socket.io")(http, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
