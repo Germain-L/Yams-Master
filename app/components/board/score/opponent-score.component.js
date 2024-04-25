b403b2130263c087d24a851d48f472f570ef8bc7
Germain-L <germain.leignel@gmail.com>
2024-04-25 11:10:10 +0200

    outsource components

---
+++ app/components/board/score/opponent-score.component.js
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+const OpponentScore = () => {
+    return (
+        <View style={styles.opponentScoreContainer}>
+            <Text>Score: </Text>
+        </View>
+    );
+};
+
+
+export default OpponentScore;
---
+++ app/components/board/score/player-score.component.js
@@ -0,0 +1,13 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+const PlayerScore = () => {
+
+    return (
+        <View style={styles.playerScoreContainer}>
+            <Text>PlayerScore</Text>
+        </View>
+    );
+};
+
+export default PlayerScore;
